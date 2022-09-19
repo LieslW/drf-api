abca84ded722151863d4efb1717563a3234e38c5
Liesl White <lieslfwhite@gmail.com>
2022-09-19 07:30:31 -0700

    framework setup

--- drf_project/drf/views.py
+++ drf_project/drf/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import Thing
+from .serializers import ThingSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+
+class ThingList(ListCreateAPIView):
+    # Cant use Django ListView bc that uses templates
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+
+class ThingDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
